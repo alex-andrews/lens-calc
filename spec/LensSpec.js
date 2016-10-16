47236bca7ad676ada04aa0194e509bedbf63df3c
alex-andrews <mr.alex.aka@gmail.com>
2016-10-16 15:30:39 +0100

    Added getters & setters for key parameters; Updated unit tests.

--- spec/LensSpec.js
+++ spec/LensSpec.js
@@ -7,52 +7,129 @@ describe('Lens', function () {
     lens = new Lens(1.74, 4.5, -5.00, 75)
   })
   describe('Check parameters supplied at instantiation', function () {
-    it('should throw an error if too few parameters are supplied', function () {
+    it('should throw an error if the wrong number of parameters are supplied', function () {
       expect(() => { lens = new Lens(1.498, 4.5, 5) })
-      .toThrow(new Error('Lens recieved too few arguments at instantiation'))
+      .toThrow(new Error('Lens requires 4 arguments at instantiation, recieved 3'))
     })
     it('should throw an error if one of the parameters is non-numeric', function () {
       expect(() => { lens = new Lens('these', 'are', 'not', 'nums') })
       .toThrow(new Error('Lens requires numerical parameters at instantiation'))
     })
   })
-  describe('Set the lens index', function () {
+  describe('Set lens index', function () {
     it('should throw an error if the value is non-numeric', function () {
-      expect(function () { lens.setIndex('string') })
+      expect(function () { lens.index = 'string' })
       .toThrow(new Error('index assignment requires numerical input'))
     })
     it('should throw an error if the value is < 1', function () {
-      expect(() => { lens.setIndex(0.5) })
+      expect(() => { lens.index = 0.5 })
       .toThrow(new Error('setIndex parameter cannot be < 1 or > 2'))
     })
     it('should throw an error if the value is > 2', function () {
-      expect(() => { lens.setIndex(3) })
+      expect(() => { lens.index = 3 })
       .toThrow(new Error('setIndex parameter cannot be < 1 or > 2'))
     })
     it('should alter the index from its initial value if not equal', function () {
-      lens.lensIndex = 1.498 // Set initial value
-      lens.setIndex(1.74) // attempt to set it
-      expect(lens.lensIndex).toBe(1.74)
+      lens._index = 1.498 // Set initial value
+      lens.index = 1.74 // attempt to set it
+      expect(lens._index).toBe(1.74)
     })
   })
-  describe('Set the blank size', function () {
+  describe('Get lens index', function () {
+    it('should return the current index value', function () {
+      let ref = 1.5
+      lens._index = ref
+      expect(lens.index).toBe(ref)
+    })
+    it('should not always return the same value', function () {
+      let ref = 1.74
+      lens._index = ref
+      expect(lens.index).toBe(ref)
+    })
+  })
+  describe('Set blank size', function () {
     it('should throw an error if the value is non-numeric', function () {
-      expect(() => { lens.setBlankSize('string') })
+      expect(() => { lens.blankSize = 'string' })
       .toThrow(new Error('blank size assignment requires numerical input'))
     })
     it('should throw an error if the blank size is < 40', function () {
-      expect(() => { lens.setBlankSize(30) })
+      expect(() => { lens.blankSize = 30 })
       .toThrow(new Error('blank size cannot be < 40 or > 85'))
     })
     it('should throw an error if the blank size is > 85', function () {
-      expect(() => { lens.setBlankSize(90) })
+      expect(() => { lens.blankSize = 90 })
       .toThrow(new Error('blank size cannot be < 40 or > 85'))
     })
     it('should alter the blank size from its initial value if not equal', function () {
-      lens.setBlankSize(70)
-      expect(lens.diameter).toBe(70)
-      lens.setBlankSize(80)
-      expect(lens.diameter).toBe(80)
+      lens.blankSize = 70
+      expect(lens._diameter).toBe(70)
+      lens.blankSize = 80
+      expect(lens._diameter).toBe(80)
+    })
+  })
+  describe('Get blank size', function () {
+    it('should return the current blank size', function () {
+      let ref = 65
+      lens._blankSize = ref
+      expect(lens.blankSize).toBe(ref)
+    })
+    it('should not always return the same value', function () {
+      let ref = 70
+      lens._blankSize = ref
+      expect(lens.blankSize).toBe(ref)
+    })
+  })
+  describe('Set base curve', function () {
+    it('should throw an error if the value is non-numeric', function () {
+      expect(() => { lens.baseCurve = 'string' })
+      .toThrow(new Error('base curve assignment requires numerical input'))
+    })
+    it('should throw an error if the value is < 0', function () {
+      expect(() => { lens.baseCurve = -5 })
+      .toThrow(new Error('base curve assignment requires values >= 0'))
+    })
+    it('should throw an error if the value is > 9', function () {
+      expect(() => { lens.baseCurve = 10 })
+      .toThrow(new Error('base curve assignment requires values <= 9'))
+    })
+    it('should alter the base curve from its initial value if not equal', function () {
+      lens.baseCurve = 2
+      expect(lens._baseCurve).toBe(2)
+      lens.baseCurve = 4
+      expect(lens._baseCurve).toBe(4)
+    }
+    )
+  })
+  describe('Get base curve', function () {
+    it('should return the current base curve', function () {
+      let ref = 3
+      lens._baseCurve = ref
+      expect(lens.baseCurve).toBe(ref)
+    })
+    it('should not always return the same value', function () {
+      let ref = 4
+      lens._baseCurve = ref
+      expect(lens.baseCurve).toBe(ref)
+    })
+  })
+  describe('Set the spherical power', function () {
+    it('should throw an error if the value is non-numeric', function () {
+      expect(() => { lens.spherePower = 'string' })
+      .toThrow(new Error('sphere power assignment requires numerical input'))
+    })
+    it('should throw an error if the value is < -20', function () {
+      expect(() => { lens.spherePower = -30 })
+      .toThrow(new Error('sphere power assignment requires values >= -20'))
+    })
+    it('should throw an error if the value is > 20', function () {
+      expect(() => { lens.spherePower = 30 })
+      .toThrow(new Error('sphere power assignment requires values <= 20'))
+    })
+    it('should alter the spherical power from its initial value if not equal', function () {
+      lens.spherePower = 5
+      expect(lens._spherePower).toBe(5)
+      lens.spherePower = 7
+      expect(lens._spherePower).toBe(7)
     })
   })
   describe('Calculate surface curvature', function () {
@@ -101,19 +178,19 @@ describe('Lens', function () {
   })
   describe('Calculate front surface power', function () {
     it('should return the correct power for a front surface', function () {
-      lens.lensIndex = 1.498
+      lens.index = 1.498
       expect((lens.frontPower).toFixed(2))
       .toBe('4.23')
     })
     it('should return a higher power for high index lenses', function () {
-      lens.lensIndex = 1.74
+      lens.index = 1.74
       expect(lens.frontPower)
       .toBeGreaterThan(4.24)
     })
   })
   describe('Calculate back surface curvature', function () {
     it('should return a power which produces the overall power when combined with the front surface', function () {
-      var overallPower = lens.power
+      var overallPower = lens.spherePower
       var frontPower = lens.frontPower
       expect((lens.backPower + lens.frontPower).toFixed(2))
       .toBe(overallPower.toFixed(2))
@@ -135,7 +212,7 @@ describe('Lens', function () {
     it('should return the correct sagitta for a spherical cap', function () {
       expect(lens.sagCalc(100, 50).toFixed(2)).toBe('3.18')
     })
-    it('should return greater sag values for larger blank sizes', function () {
+    it('should return greater sagitta values for larger blank sizes', function () {
       var reference = lens.sagCalc(100, 50)
       expect(lens.sagCalc(100, 70)).toBeGreaterThan(reference)
     })
--- src/Lens.js
+++ src/Lens.js
@@ -1,37 +1,71 @@
 class Lens { // eslint-disable-line no-unused-vars
-  constructor (lensIndex, baseCurve, power, diameter) {
+  constructor (lensIndex, baseCurve, spherePower, diameter) {
     if (arguments.length !== 4) {
-      throw new Error('Lens recieved too few arguments at instantiation')
+      throw new Error('Lens requires 4 arguments at instantiation, recieved ' + arguments.length)
     } else if (
       typeof (lensIndex) !== 'number' ||
       typeof (baseCurve) !== 'number' ||
-      typeof (power) !== 'number' ||
+      typeof (spherePower) !== 'number' ||
       typeof (diameter) !== 'number'
     ) {
       throw new Error('Lens requires numerical parameters at instantiation')
     }
-    this.setIndex(lensIndex)
-    this.baseCurve = baseCurve
-    this.power = power
-    this.setBlankSize(diameter)
+    this._index = lensIndex
+    this._blankSize = diameter
+    this._baseCurve = baseCurve
+    this._spherePower = spherePower
   }
-  setIndex (newIndex) {
-    if (typeof (newIndex) !== 'number') {
+  set index (i) {
+    if (typeof (i) !== 'number') {
       throw new Error('index assignment requires numerical input')
-    } else if (newIndex < 1 || newIndex > 2) {
+    } else if (i < 1 || i > 2) {
       throw new Error('setIndex parameter cannot be < 1 or > 2')
     } else {
-      this.lensIndex = newIndex
+      this._index = i
     }
   }
-  setBlankSize (newDiameter) {
-    if (typeof (newDiameter) !== 'number') {
+  get index () {
+    return this._index
+  }
+  set blankSize (diameter) {
+    if (typeof (diameter) !== 'number') {
       throw new Error('blank size assignment requires numerical input')
-    } else if (newDiameter < 40 || newDiameter > 85) {
+    } else if (diameter < 40 || diameter > 85) {
       throw new Error('blank size cannot be < 40 or > 85')
     } else {
-      this.diameter = newDiameter
+      this._diameter = diameter
     }
+  }
+  get blankSize () {
+    return this._blankSize
+  }
+  set baseCurve (bc) {
+    if (typeof (bc) !== 'number') {
+      throw new Error('base curve assignment requires numerical input')
+    } else if (bc < 0) {
+      throw new Error('base curve assignment requires values >= 0')
+    } else if (bc > 9) {
+      throw new Error('base curve assignment requires values <= 9')
+    } else {
+      this._baseCurve = bc
+    }
+  }
+  get baseCurve () {
+    return this._baseCurve
+  }
+  set spherePower (power) {
+    if (typeof (power) !== 'number') {
+      throw new Error('sphere power assignment requires numerical input')
+    } else if (power < -20) {
+      throw new Error('sphere power assignment requires values >= -20')
+    } else if (power > 20) {
+      throw new Error('sphere power assignment requires values <= 20')
+    } else {
+      this._spherePower = power
+    }
+  }
+  get spherePower () {
+    return this._spherePower
   }
   curvatureCalc (lensIndex, power) {
     if (typeof (lensIndex) !== 'number' || typeof (power) !== 'number') {
@@ -41,7 +75,7 @@ class Lens { // eslint-disable-line no-unused-vars
     } else if (lensIndex <= 0) {
       throw new Error('index must be greater than 0')
     } else {
-      return (((lensIndex - 1) / power) * 1000)
+      return Math.abs((((lensIndex - 1) / power) * 1000))
     }
   }
   surfacePowerCalc (lensIndex, curvature) {
@@ -66,13 +100,20 @@ class Lens { // eslint-disable-line no-unused-vars
     var blankRadius = blankDiameter / 2
     var i = Math.pow(curvature, 2) - Math.pow(blankRadius, 2)
     var sag = curvature - Math.sqrt(i)
-    console.log(sag)
     return sag
   }
   get frontPower () {
-    return this.surfacePowerCalc(this.lensIndex, this.curvatureCalc(1.53, this.baseCurve))
+    return this.surfacePowerCalc(this._index, this.curvatureCalc(1.53, this._baseCurve))
   }
   get backPower () {
-    return this.power - this.frontPower
+    return this._spherePower - this.frontPower
+  }
+  get frontSag () {
+    var frontCurvature = this.curvatureCalc(1.530, this._baseCurve)
+    return this.sagCalc(frontCurvature, this._diameter)
+  }
+  get backSag () {
+    var backCurvature = this.curvatureCalc(this._index, this.backPower)
+    return this.sagCalc(backCurvature, this._diameter)
   }
 }
